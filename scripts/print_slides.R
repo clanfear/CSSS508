bf64661115503a6858d988ddf1c8aaa0bd3e6f88
Charles Lanfear <cclanfear@gmail.com>
2019-04-03 14:16:56 -0700

    Some encoding fuckery

--- scripts/print_slides.R
+++ scripts/print_slides.R
@@ -2,7 +2,7 @@ render_and_print_slides <- function(week){
   if (Sys.info()["user"]=="cclan"){
     week_dir     <- paste0(getwd(), "/Lectures/", "Week", week, "/")
     current_rmd  <- paste0(week_dir, stringr::str_subset(list.files(week_dir), "^CSSS508_Week.*\\.Rmd$"))
-    rmarkdown::render(current_rmd)
+    rmarkdown::render(current_rmd, encoding = "UTF-8")
     current_html <- stringr::str_replace(current_rmd, "\\.Rmd", "\\.html")
     new_file     <- stringr::str_replace(current_html, "\\.html", "\\.pdf")
     Sys.sleep(5)
